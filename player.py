45e344037afd5ec6a50fe6fcd5b604a213d9215b
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-11-09 12:28:13 -0700

    added input file to keep track fo keys pressed

--- player.py
+++ player.py
@@ -4,3 +4,7 @@ from sprite import Sprite
 class Player(Sprite):
     def __init__(self, image, x, y):
         super().__init__(image, x, y)
+        self.movement_speed = 2
+
+    def update(self):
+        

8a43bc16a4257522a04e52dbb74c383806a5c273
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-11-09 12:24:26 -0700

    added sprites file

---
+++ sprites.py
@@ -0,0 +1,19 @@
+sprites = []
+loaded = {}
+
+class Sprite:
+    def __init__(self, image, x, y):
+        if image in loaded:
+            self.image = loaded[image]
+        else:
+            self.image = pygame.image.load(image)
+            loaded[image] = self.image
+        self.x = x
+        self.y = y
+        sprites.append(self)
+
+    def delete(self):
+        sprites.remove(self)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
